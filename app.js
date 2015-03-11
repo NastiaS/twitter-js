6a628a19612b6c219694eeda70448d96f3b587a3
NastiaS <anastasiasergienko@gmail.com>
2015-03-11 16:02:29 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require( 'express' );
+var app = express();
+var morgan = require('morgan');
+app.use( morgan('dev') );
+
+app.get('/', function (req, res) {
+  res.send('Hello World!' + "Yep, it works!")
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("server listening and I am modifying something", host, port)
+
+});
